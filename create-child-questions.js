05d4f76e06b02536cfc89643b62cb14ae2aae993
Adrian Harris <60455439+Hadrian77@users.noreply.github.com>
2022-07-06 01:35:43 -0400

    Updating Child Question Page

--- create-child-questions.js
+++ create-child-questions.js
@@ -1,13 +1,13 @@
+const urlDomain = "https://pubby-service.herokuapp.com";
+let questionCount = 0;
 let answerCount = 1;
 let tagCount = 1;
 let keywordCount = 1;
-const urlDomain = "https://pubby-service.herokuapp.com";
 
 function createQuestion() {
   const parameterMap = getParameters();
 
   let question = {
-    id: parameterMap.get("id"),
     type: parameterMap.get("type"),
     description: parameterMap.get("description"),
     pack: parameterMap.get("pack"),
@@ -15,6 +15,8 @@ function createQuestion() {
     keywords: getKeywordArray(parameterMap),
     tags: getTagArray(parameterMap),
   };
+
+  sessionStorage.setItem("parentQuestion", question);
 
   requestData = {
     method: "POST",
@@ -49,6 +51,15 @@ function getParameters() {
   return map;
 }
 
+function getAnswerArray(parameterMap) {
+  let answers = new Array();
+  for (let i = 0; i < answerCount; i++) {
+    answers.push(parameterMap.get("answer" + answerCount));
+  }
+  console.log(answers);
+  return answers;
+}
+
 function getTagArray(parameterMap) {
   let tags = new Array();
   for (let i = 0; i < tagCount; i++) {
@@ -65,6 +76,48 @@ function getKeywordArray(parameterMap) {
   }
   console.log(keywords);
   return keywords;
+}
+
+function addQuestion(description) {
+  const questionsElement = document.querySelector(".questions");
+
+  questionCount++;
+  let questionElement = document.createElement("textarea");
+  let questionName = "question" + questionCount;
+
+  let questionLabelElement = document.createElement("label");
+  questionLabelElement.for = questionName;
+  questionLabelElement.textContent = "Question " + questionCount;
+
+  questionElement.classList.add("answer");
+  questionElement.name = questionName;
+  questionElement.id = questionName;
+  questionElement.rows = "4";
+  questionElement.cols = "40";
+  questionElement.form = "questionForm";
+  questionElement.textContent = description;
+
+  questionsElement.appendChild(questionLabelElement);
+  questionsElement.appendChild(document.createElement("br"));
+  questionsElement.appendChild(questionElement);
+  questionsElement.appendChild(document.createElement("br"));
+}
+
+function displayQuestionTemplates(questionType) {
+  const questionDescription = sessionStorage.getItem("question").description;
+  switch (questionType) {
+    case "left_or_right":
+      const leftOrRightGuess =
+        "How would Adrian respond to the following prompt:\n " +
+        questionDescription;
+      addQuestion(leftOrRightGuess);
+      break;
+    case y:
+      // code block
+      break;
+    default:
+    // code block
+  }
 }
 
 createQuestion();
